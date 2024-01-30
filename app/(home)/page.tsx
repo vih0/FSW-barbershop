cb54040b4ef21ed3df81310542fb283909fe8dbc
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-01-30 11:16:47 -0300

    feat: add hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import {format} from 'date-fns'
 import Header from "../_components/Header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
   <div>
     <Header/>
+    <div className="px-5 pt-5">
+    <h2 className="text-xl font-bold">Ola,Miguel</h2>
+    <p className="text-sm">{format(Date(),"EEEE',' d 'de' MMMM",{
+      locale:ptBR
+    })}</p>
+    </div>
   </div>
   );
 }
