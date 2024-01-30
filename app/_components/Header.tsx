ccedd28dc9edb7a2abd7141ac39eb77180765e14
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-01-30 10:52:35 -0300

    feat: initial header component

---
+++ app/_components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+const Header = ()=>{
+    return(
+        <Card>
+            <CardContent className="px-5 py-8 flex justify-between flex-row">
+            <Image src="/LogoBarber.svg" alt="FSW Baber" height={22} width={120}/>
+             <Button variant="outline" size="icon" className="h-8 w-8">
+                <HamburgerMenuIcon />
+                </Button>   
+            </CardContent>
+
+        </Card>
+    )
+}
+export default Header;
