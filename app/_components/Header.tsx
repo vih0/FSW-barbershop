5eeec82a4e81b06db386bc19f01cc51e05f1544d
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-02-01 17:57:51 -0300

    feat: add Auth provider

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,19 +1,24 @@
+"use client";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { signIn } from "next-auth/react";
 
-const Header = ()=>{
-    return(
-        <Card>
-            <CardContent className="px-5 py-8 flex justify-between flex-row">
-            <Image src="/LogoBarber.svg" alt="FSW Baber" height={22} width={120}/>
-             <Button variant="outline" size="icon" className="h-8 w-8">
+const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  return (
+    <Card>
+      <CardContent className="px-5 py-8 flex justify-between flex-row">
+        <Image src="/LogoBarber.svg" alt="FSW Baber" height={22} width={120} />
+        {/* <Button variant="outline" size="icon" className="h-8 w-8">
                 <HamburgerMenuIcon />
-                </Button>   
-            </CardContent>
-
-        </Card>
-    )
-}
+                </Button>    */}
+        <Button onClick={handleLoginClick}>login</Button>
+      </CardContent>
+    </Card>
+  );
+};
 export default Header;
